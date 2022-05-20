cff908cab3810094f8565e5de105929f8311ed89
Yao Xie <yao.xie@niceincontact.com>
2022-05-20 19:00:36 -0400

    test code build cicd

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ exports.handler = async (event, context) => {
     };
     try {
         console.log("The date and time are currently: " + dt.currentDateTime());
-        //const { ContentType } = await s3.getObject(params).promise();
-        //console.log('CONTENT TYPE:', ContentType);
-        //return ContentType;
+        const { ContentType } = await s3.getObject(params).promise();
+        console.log('CONTENT TYPE:', ContentType);
+        return ContentType;
     } catch (err) {
         console.log(err);
         const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
